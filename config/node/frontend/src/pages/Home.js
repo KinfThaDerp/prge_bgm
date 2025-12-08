544dadcd707fcf70f4e67d3c21e147b62693151c
KinfThaDerp <bassam.grzegorz@gmail.com>
2025-12-08 19:59:20 +0100

    feat: content of home component and style for it

--- config/node/frontend/src/pages/Home.js
+++ config/node/frontend/src/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {Typography, Button} from '@mui/material';
 
 function Home(props) {
     return (
-        <div> strona startowa react
+        <div ClassName="home">
+            <h1 className="home__title">GEOPORTAL</h1>
+            <Typography classname='home__subtitle'>
+                Geoportal tematyczny poświęcony danym przestrzennym przetwarzanym
+            </Typography>
+
+            <Button className="home__button" variant='contained' size='large'> START </Button>
         </div>
     );
 }
